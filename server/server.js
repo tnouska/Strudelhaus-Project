cd691e5f0bcf1d4d2eac4b87764fbe7afdd62502
Teagan Nouska <tnouska@live.com>
2018-05-21 15:19:40 -0500

    admin, customer, org.leader base route pages added

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const adminRouter = require('./routes/admin.router');
+const orgLeaderRouter = require('./routes/org.leader.router');
+const customerRouter = require('./routes/customer.router')
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,6 +27,9 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/admin', adminRouter);
+app.use('/api/orgleader', orgLeaderRouter);
+app.use('/api/customer', customerRouter);
 
 // Serve static files
 app.use(express.static('build'));
