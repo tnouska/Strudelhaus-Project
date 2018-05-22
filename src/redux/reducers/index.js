15e834efcac62b321f8807de6320c31b3813b51a
Joshua John <JoshuaLeary@Danicas-MacBook-Pro.local>
2018-05-22 09:42:00 -0500

    Added pipelineSaga, Added pipelineReducer, for GET_PIPELINE

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import user from './userReducer';
 import login from './loginReducer';
+import pipeline from './pipelineReducer';
 
 const store = combineReducers({
   user,
   login,
+  pipeline,
 });
 
 export default store;
