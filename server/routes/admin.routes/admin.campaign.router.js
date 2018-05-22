92e89eeda076c3a8307797ec525ec3dcc3765f32
Teagan Nouska <tnouska@live.com>
2018-05-22 07:21:10 -0500

    base layout for admin.campaign.router.get added

--- server/routes/admin.routes/admin.campaign.router.js
+++ server/routes/admin.routes/admin.campaign.router.js
@@ -7,8 +7,30 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-
-});
+    if (req.isAuthenticated()) {
+        //checking if user is authenticated
+        (async()=>{
+            //creates async function
+            const client = await pool.connect();
+            //await will wait for a return on the given function and then do something
+            try {
+                
+            } catch (error) {
+                console.log('ROLLBACK', error);
+                await client.query('ROLLBACK');
+                throw error; 
+            } finally {
+                client.release();   
+            };//end try/catch/finally
+        })().catch((error)=>{
+            console.log('error in CATCH: ',error);
+            res.sendStatus(500);
+        });//end async/await
+    } else {
+        res.sendStatus(403);
+        //if user is not authenticated will send unauthorized
+    };//end authorization if/else
+});//end admin.campaign.router.get
 
 /**
  * POST route template
