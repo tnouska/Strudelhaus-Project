e7c65a99c3d6551ed40ab0fb928556edbe77e5f5
Teagan Nouska <tnouska@live.com>
2018-05-23 13:32:32 -0500

    customer.storefront.router GET completed

--- server/routes/customer.routes/customer.storefront.router.js
+++ server/routes/customer.routes/customer.storefront.router.js
@@ -6,9 +6,46 @@ const router = express.Router();
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-
-});
+router.get('/:id', (req, res) => {
+    (async()=>{
+    //creates async function
+        const client = await pool.connect();
+        //await will wait for a return on the given function and then do something
+        try {
+            await client.query('BEGIN');            
+            let queryText = `SELECT
+                campaign.id
+                from campaign
+                WHERE campaign.url = $1;`
+            const campaignId = await client.query(queryText, [req.params.id])                        
+            let queryText2 = `SELECT
+                product.name as product_name,
+                product.price as product_price,
+                product.description as product_description,
+                product.img_url_1,
+                product.img_url_2,
+                product_type.type as product_type
+                FROM available_item
+                JOIN product ON available_item.product_id = product.id
+                JOIN product_type ON product.product_type = product_type.id
+                WHERE campaign_id = $1
+                ORDER BY product_type desc, product_name asc;`
+            const availableItems = await client.query(queryText2, [campaignId.rows[0].id])            
+            await client.query('COMMIT');
+            res.send(availableItems.rows)
+        } catch (error) {
+            console.log('ROLLBACK', error);
+            await client.query('ROLLBACK');
+            throw error;
+        } finally {
+            client.release();
+            //will release end query('BEGIN')
+        };// end try/catch
+    })().catch((error)=>{
+        console.log('CATCH', error);
+        res.sendStatus(500);
+    })//end async
+});//end router.get for products
 
 /**
  * POST route template
