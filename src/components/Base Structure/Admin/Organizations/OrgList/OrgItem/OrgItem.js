3ad57e43897da6583f2f49f6a179e856542f9061
Steve Hogan <stevenjameshogan@gmail.com>
2018-05-24 11:04:56 -0500

    Org panel working

--- src/components/Base Structure/Admin/Organizations/OrgList/OrgItem/OrgItem.js
+++ src/components/Base Structure/Admin/Organizations/OrgList/OrgItem/OrgItem.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Button, Panel } from 'react-bootstrap';
 import OrgItemCampaign from './OrgItemCampaign/OrgItemCampaign';
 
 class OrgItem extends Component {
+    constructor(props){
+        super(props);
+        this.state = ({
+            panelOpen: false
+        })
+    }
 
     deleteOrg = () => {
         console.log('org:', this.props.org);
@@ -19,13 +26,29 @@ class OrgItem extends Component {
         })
 
         return(
-            <div className="orgItemDiv">
-                <p>{this.props.org.organization_name}</p>
-                <p>Contact: {this.props.org.contact_name}</p>
-                <p>Campaigns</p>
-                {campaignList}
-                <button>Edit</button>
-                <button onClick={this.deleteOrg}>Delete</button>
+            <div>
+                <Panel id="collapsible-panel-example-2">
+                    <Panel.Heading>
+                        <Panel.Title toggle>
+                        {this.props.org.organization_name}
+                        </Panel.Title>
+                    </Panel.Heading>
+                    <Panel.Collapse>
+                        <Panel.Body>
+                        Contact: {this.props.org.contact_name}
+                        <p>Campaigns</p>
+                        {campaignList}
+                        <Button>Edit</Button>
+                        <Button onClick={this.deleteOrg}>Delete</Button> 
+                        </Panel.Body>
+                    </Panel.Collapse>
+                </Panel>
+                {/* // <p>{this.props.org.organization_name}</p>
+                // <p>Contact: {this.props.org.contact_name}</p>
+                // <p>Campaigns</p>
+                // {campaignList}
+                // <button>Edit</button>
+                // <button onClick={this.deleteOrg}>Delete</button> */}
             </div>
         )
     }
